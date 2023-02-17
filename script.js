667369cf9ae1114d162177c565bafb4498ca2fcb
Asbeyy <114877785+Asbeyy@users.noreply.github.com>
2023-02-17 19:00:40 +0100

    second commit

---
+++ script.js
@@ -0,0 +1,38 @@
+const canva = document.querySelector(".canva");
+const button = document.getElementById("button")
+const input = document.getElementById("input")
+let counter = 0;
+let stat = 1;
+
+function createString(testo){
+    counter += 1
+    stat+= 1
+    let child = document.createElement("div")
+    canva.appendChild(child)
+    child.setAttribute("id",`test${counter}`);
+    child.setAttribute("class","test")
+    child.innerHTML = testo
+    changeColor(child)
+  
+}
+
+
+function changeColor(child){
+    if (stat % 2 === 0 ){
+        child.setAttribute("class","test")
+    } else {
+        child.setAttribute("class","test-receive")
+    }
+}
+
+button.onclick = function(){
+    createString(input.value)
+}
+
+document.addEventListener("keypress", function(event) {
+    if (event.keyCode == 13) {
+        createString(input.value)
+        input.value = ""
+    }
+});
+//createString("TEST")
